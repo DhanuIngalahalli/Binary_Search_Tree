5f1dea3d42f3cf131929334f726ce85cf354a89a
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-09-25 09:41:17 -0700

    welcome

---
+++ BTS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to binary search tree");
+        }
+    }
+}
