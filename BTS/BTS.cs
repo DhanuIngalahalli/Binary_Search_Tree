a7c9e4744ed6960f277a2d13b41888b373478476
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-09-25 09:50:52 -0700

    create BST

---
+++ BTS/BTS.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BST
+{
+    class BST<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BST<T> LeftTree { get; set; }
+        public BST<T> RightTree { get; set; }
+
+        //define constructor
+        public BST(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.RightTree = null;
+            this.LeftTree = null;
+        }
+        /// <summary>
+        /// to add data in BST 
+        /// </summary>
+        /// <param name="item"></param>
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BST<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BST<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+        private int leftCount = 0, rightCount = 0;
+        /// <summary>
+        /// to display data
+        /// </summary>
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+    }
+}
--- BTS/Program.cs
+++ BTS/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace BTS
+namespace BST
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to binary search tree");
+            BST<int> binarySearch = new BST<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
         }
     }
 }
